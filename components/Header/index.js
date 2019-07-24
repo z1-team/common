086c8535e9fe41f8b6f28c173c32080e4774a359
Aleksey <alekseyvi1303@gmail.com>
2019-07-24 13:58:36 +0300

    Update: components, styles - Header, Footer

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,9 +7,6 @@ const Header = ({place, showBot, class: mixin, children}) => (
 	<div class={`${style.wrHeader} ${mixin ? mixin : ''}`}>
 		<div class="container">
 			<div class={style.header}>
-				<div class={style.contacts}>
-					<p class={style.contactsText}><Icon icon="marker" />{place}</p>
-				</div>
 				{children}
 			</div>
 		</div>
