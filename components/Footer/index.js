d873a6d9f81d62ad02ec5653d536c3c7173f3c5f
Aleksey <alekseyvi1303@gmail.com>
2019-07-25 10:53:23 +0300

    Update: components - Header, Footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,4 @@
 import { h, Component } from 'preact'
-import { Link } from 'preact-router/match'
 
 import style from './style.scss'
 
@@ -11,18 +10,12 @@ class Footer extends Component {
 
 		onLogin(isLoggedIn)
 	}
-	render({isLoggedIn, logo, class: mixin}) {
+	render({isLoggedIn, logo, class: mixin, children}) {
     return (
       <div class={`${style.wrFooter} ${mixin ? mixin : ''}`} id="footer">
         <div class="container">
           <div class={style.footer}>
-            <span class={style.logo}><img src={logo}/></span>
-            <small>&copy; 2019 Moneyonline &mdash; все займы онлайн на карту или наличными в одном месте. 16+</small>
-            <ul class={style.admin}>
-              <li><button onClick={this.handleClick}>{isLoggedIn ? "Выйти" : "Войти"}</button></li>
-              {isLoggedIn && <li><Link href="/moderate">Модерация отзывов</Link></li>}
-              <li><Link activeClassName="active" href="/confidentiality">Политика конфиденциальности</Link></li>
-            </ul>
+            {children}
           </div>
         </div>
       </div>
--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@ import Icon from 'common/components/Icon'
 
 import style from './style.scss'
 
-const Header = ({place, showBot, class: mixin, children}) => (
+const Header = ({class: mixin, children}) => (
 	<div class={`${style.wrHeader} ${mixin ? mixin : ''}`}>
 		<div class="container">
 			<div class={style.header}>
