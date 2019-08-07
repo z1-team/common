6873f403bb2599cb0d44d6d7b5399472be241c2b
Aleksey <alekseyvi1303@gmail.com>
2019-08-07 15:26:19 +0300

    Update: components - PopupWrap

--- components/PopupWrap/index.js
+++ components/PopupWrap/index.js
@@ -2,8 +2,8 @@ import { h } from 'preact'
 
 import style from './style.scss'
 
-const Popups = ({isOpened, children, onClose}) => (
-  <div class={isOpened ? `${style.active}` : `${style.normal}`} onClick={onClose}>
+const Popups = ({isOpened, children, onClose, class: mixin}) => (
+  <div class={`${isOpened ? style.active : style.normal} ${mixin ? mixin : ''}`} onClick={onClose}>
     {children}
   </div>
 )
