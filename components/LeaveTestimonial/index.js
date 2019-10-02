95d81b25b7522891dbba165da6ed9b4464479715
Aleksey <alekseyvi1303@gmail.com>
2019-10-02 10:25:36 +0300

    Debug: leave testimonials

--- components/LeaveTestimonial/index.js
+++ components/LeaveTestimonial/index.js
@@ -50,8 +50,8 @@ class Testi extends Component {
     const {id, onSubmit} = this.props
 
     if(name && email && text && rating !== 0) {
+      console.log({partner: id, name, email, text, rating}, onSubmit)
       if (typeof onSubmit === 'function') {
-        console.log({partner: id, name, email, text, rating})
         onSubmit({partner: id, name, email, text, rating})
       }
 
